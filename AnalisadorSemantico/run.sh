ba5c0cff27789622b956179e4fa9f0e63b3d2cd9
luis-sribeiro <72897006+luis-sribeiro@users.noreply.github.com>
2021-08-18 23:11:50 -0300

    Seperando o build do run

--- AnalisadorSemantico/run.sh
+++ AnalisadorSemantico/run.sh
@@ -1,6 +1,1 @@
-java -jar lang/parser/JFlex.jar -nobak lang/parser/Lang.flex
-java -jar lang/parser/beaver-cc-0.9.11.jar -T lang/parser/Lang.grammar
-javac -cp .:./beaver-rt-0.9.11.jar lang/semanticanalysis/*.java
-javac -cp .:./beaver-rt-0.9.11.jar lang/visitor/*.java
-javac -cp .:beaver-rt-0.9.11.jar lang/LangCompiler.java
 java -cp .:./beaver-rt-0.9.11.jar lang/LangCompiler $1 $2
